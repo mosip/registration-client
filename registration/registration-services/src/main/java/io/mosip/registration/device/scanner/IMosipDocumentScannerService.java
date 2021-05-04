b4b911ed9fb078310c2c90b824f6020c3e235fb7
Sudip Banerjee <Neosoft_SudipB@nsdl.co.in>
2021-05-04 15:29:57 +0530

    MOSIP-12465, cleaning commnted code

--- registration/registration-services/src/main/java/io/mosip/registration/device/scanner/IMosipDocumentScannerService.java
+++ registration/registration-services/src/main/java/io/mosip/registration/device/scanner/IMosipDocumentScannerService.java
@@ -30,49 +30,6 @@ public interface IMosipDocumentScannerService {
 	BufferedImage scan();
 
 	/**
-	 * This method converts the separate scanned file into single document
-	 * 
-	 * @param bufferedImages
-	 *            - the scanned files
-	 * @return byte[] - The single document which contains all the scanned files
-	 */
-//	byte[] asPDF(List<BufferedImage> bufferedImages);
-
-	/**
-	 * This method is used to merge all the scanned images into single image side by
-	 * side
-	 * 
-	 * @param bufferedImages
-	 *            - holds all the separate scanned pages
-	 * @return byte[] - The final single file which contains all the scanned files
-	 * @throws IOException
-	 *             - holds the ioexception
-	 */
-//	byte[] asImage(List<BufferedImage> bufferedImages) throws IOException;
-
-	/**
-	 * This method converts the BufferedImage to byte[]
-	 * 
-	 * @param bufferedImage
-	 *            - holds the scanned image from the scanner
-	 * @return byte[] - scanned document Content
-	 * @throws IOException
-	 *             - holds the ioexcepion
-	 */
-//	byte[] getImageBytesFromBufferedImage(BufferedImage bufferedImage) throws IOException;
-
-	/**
-	 * This method is used to convert the pdf file into images
-	 * 
-	 * @param pdfBytes
-	 *            - pdf file in bytes
-	 * @return List - holds the list of buffered images
-	 * @throws IOException
-	 *             - holds the ioexception
-	 */
-//	List<BufferedImage> pdfToImages(byte[] pdfBytes) throws IOException;
-
-	/**
 	 *
 	 * @return
 	 */
