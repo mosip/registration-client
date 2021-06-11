778e8acf09c25b4940aa1c94f61e441a8dad7407
ase-101 <sunkadaeanusha@gmail.com>
2021-06-11 15:45:18 +0530

    Update configure.sh

--- registration/configure.sh
+++ registration/configure.sh
@@ -95,7 +95,7 @@ mkdir -p "${work_dir}"/registration-test-utility/lib
 cp "${work_dir}"/registration-test/target/registration-test-*-dependencies.jar "${work_dir}"/registration-test-utility/registration-test.jar
 cp "${work_dir}"/registration-test/resources/*  "${work_dir}"/registration-test-utility/
 cp -r "${work_dir}"/registration-libs/resources/jre "${work_dir}"/registration-test-utility/
-cp -r "${work_dir}"/registration-client/target/lib/morena* "${work_dir}"/registration-test-utility/lib
+#cp -r "${work_dir}"/registration-client/target/lib/morena* "${work_dir}"/registration-test-utility/lib
 cp -r "${work_dir}"/registration-client/target/lib/icu4j.jar "${work_dir}"/registration-test-utility/lib
 cp -r "${work_dir}"/registration-client/target/lib/kernel-transliteration-icu4j.jar "${work_dir}"/registration-test-utility/lib
 cp -r "${work_dir}"/registration-client/target/lib/clamav.jar "${work_dir}"/registration-test-utility/lib
