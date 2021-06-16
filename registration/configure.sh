f2457755d9f78e389d233eb7cc266daa138e4aa9
Neeharika Garg <Neeharika.Garg@technoforte.co.in>
2021-06-16 15:49:54 +0530

    configure.sh for work dir in reg test and set id for group checkbox

--- registration/configure.sh
+++ registration/configure.sh
@@ -102,7 +102,9 @@ cp -r "${work_dir}"/registration-client/target/lib/clamav.jar "${work_dir}"/regi
 cp -r "${work_dir}"/registration-client/target/lib/kernel-virusscanner-clamav.jar "${work_dir}"/registration-test-utility/lib
 cp -r "${work_dir}"/sdkjars/*.jar "${work_dir}"/registration-test-utility/lib
 cp "${work_dir}"/registration-client/target/MANIFEST.MF "${work_dir}"/registration-test-utility/
-/usr/bin/zip -r "${work_dir}"/registration-test-utility.zip "${work_dir}"/registration-test-utility
+
+cd "${work_dir}"
+/usr/bin/zip -r ./registration-test-utility.zip ./registration-test-utility/*
 
 echo "setting up nginx static content"
 
