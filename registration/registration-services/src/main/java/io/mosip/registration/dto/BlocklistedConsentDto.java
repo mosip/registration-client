35f26dfdb80b42a24139efcc8079cb79788f8e4c
ase-101 <>
2021-08-16 15:26:13 +0530

    Added blocklisted words dto

--- registration/registration-services/src/main/java/io/mosip/registration/dto/BlocklistedConsentDto.java
+++ registration/registration-services/src/main/java/io/mosip/registration/dto/BlocklistedConsentDto.java
@@ -1,4 +1,12 @@
 package io.mosip.registration.dto;
 
+import java.util.List;
+
 public class BlocklistedConsentDto {
+
+    private String screenName;
+    private List<String> words;
+    private Boolean operatorConsent;
+    private String supervisorId;
+    private String supervisorAuthType;
 }
