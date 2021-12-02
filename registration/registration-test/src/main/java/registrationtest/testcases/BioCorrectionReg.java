a50fce9ac9a7eeaa9fa037222c7aa88ee1138f31
Neeharika Garg <Neeharika.Garg@technoforte.co.in>
2021-12-02 21:38:58 +0530

    added wait and trim

--- registration/registration-test/src/main/java/registrationtest/testcases/BioCorrectionReg.java
+++ registration/registration-test/src/main/java/registrationtest/testcases/BioCorrectionReg.java
@@ -173,7 +173,7 @@ public class BioCorrectionReg {
 
             eodApprovalPage = homePage.clickeodApprovalImageView(applicationPrimaryStage, scene);
             eodApprovalPage.clickOnfilterField();
-            eodApprovalPage.enterFilterDetails(rid1.getRid());
+            eodApprovalPage.enterFilterDetails(rid1.getRid().trim());
             eodApprovalPage.clickOnApprovalBtn();
             authenticationPage = eodApprovalPage.clickOnAuthenticateBtn();
             authenticationPage.enterUserName(supervisorUserid);
@@ -196,12 +196,12 @@ public class BioCorrectionReg {
             if (PropertiesUtil.getKeyValue("upload").equals("Y")) {
 
                 uploadPacketPage = homePage.clickuploadPacketImageView(applicationPrimaryStage, scene);
-                uploadPacketPage.selectPacket(rid1.getRid());
+                uploadPacketPage.selectPacket(rid1.getRid().trim());
                 buttons.clickuploadBtn();
                 /**
                  * Verify Success Upload
                  */
-                result = uploadPacketPage.verifyPacketUpload(rid1.getRid());
+                result = uploadPacketPage.verifyPacketUpload(rid1.getRid().trim());
                 ExtentReportUtil.test1.info("Upload Packet done");
             } else if (PropertiesUtil.getKeyValue("upload").equals("N")) {
                 result = true;
