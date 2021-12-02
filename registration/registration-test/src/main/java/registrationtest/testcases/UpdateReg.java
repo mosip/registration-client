4b70efe77ea6c51cc8c043642fdcd97ead345a7f
Neeharika Garg <Neeharika.Garg@technoforte.co.in>
2021-12-02 21:38:58 +0530

    added wait and trim

--- registration/registration-test/src/main/java/registrationtest/testcases/UpdateReg.java
+++ registration/registration-test/src/main/java/registrationtest/testcases/UpdateReg.java
@@ -171,7 +171,7 @@ public class UpdateReg {
 
             eodApprovalPage = homePage.clickeodApprovalImageView(applicationPrimaryStage, scene);
             eodApprovalPage.clickOnfilterField();
-            eodApprovalPage.enterFilterDetails(rid1.getRid());
+            eodApprovalPage.enterFilterDetails(rid1.getRid().trim());
             eodApprovalPage.clickOnApprovalBtn();
             authenticationPage = eodApprovalPage.clickOnAuthenticateBtn();
             authenticationPage.enterUserName(supervisorUserid);
@@ -194,12 +194,12 @@ public class UpdateReg {
             if (PropertiesUtil.getKeyValue("upload").equals("Y")) {
 
                 uploadPacketPage = homePage.clickuploadPacketImageView(applicationPrimaryStage, scene);
-                uploadPacketPage.selectPacket(rid1.getRid());
+                uploadPacketPage.selectPacket(rid1.getRid().trim());
                 buttons.clickuploadBtn();
                 /**
                  * Verify Success Upload
                  */
-                result = uploadPacketPage.verifyPacketUpload(rid1.getRid());
+                result = uploadPacketPage.verifyPacketUpload(rid1.getRid().trim());
                 ExtentReportUtil.test1.info("Upload Packet done");
             } else if (PropertiesUtil.getKeyValue("upload").equals("N")) {
                 result = true;
