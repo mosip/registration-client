611f73b1cac20f0c7fab14a236baad92018bdd7f
ase-101 <>
2021-12-15 19:31:30 +0530

    client setup validation issue fix in test-utility

--- registration/registration-client/src/main/java/io/mosip/registration/controller/Initialization.java
+++ registration/registration-client/src/main/java/io/mosip/registration/controller/Initialization.java
@@ -6,6 +6,9 @@ import io.mosip.registration.preloader.ClientPreLoader;
 public class Initialization {
 
     public static void main(String[] args) {
+        System.setProperty("java.net.useSystemProxies", "true");
+        System.setProperty("file.encoding", "UTF-8");
+        System.setProperty("logback.configurationFile", "lib/logback.xml");
         LauncherImpl.launchApplication(ClientApplication.class, ClientPreLoader.class, args);
     }
 }
