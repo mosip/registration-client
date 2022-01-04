9df80f8e904a37fed704debbae3988523f2107e6
M1063027 <M1063027@C1ML45695.mindtree.com>
2022-01-04 19:57:53 +0530

    push the code for as part of the sonar coverage

--- registration/registration-services/src/test/java/io/mosip/registration/test/dao/impl/RegistrationCenterDAOTest.java
+++ registration/registration-services/src/test/java/io/mosip/registration/test/dao/impl/RegistrationCenterDAOTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Optional;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -12,11 +13,18 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.powermock.api.mockito.PowerMockito;
 
+import io.mosip.registration.context.ApplicationContext;
+import io.mosip.registration.context.SessionContext;
+import io.mosip.registration.context.SessionContext.UserContext;
 import io.mosip.registration.dao.impl.RegistrationCenterDAOImpl;
+import io.mosip.registration.entity.MachineMaster;
 import io.mosip.registration.entity.RegistrationCenter;
 import io.mosip.registration.entity.id.RegistartionCenterId;
+import io.mosip.registration.repositories.MachineMasterRepository;
 import io.mosip.registration.repositories.RegistrationCenterRepository;
+import io.mosip.registration.util.healthcheck.RegistrationSystemPropertiesChecker;
 
 public class RegistrationCenterDAOTest {
 
@@ -28,7 +36,20 @@ public class RegistrationCenterDAOTest {
 
 	@Mock
 	private RegistrationCenterRepository registrationCenterRepository;
+	
+	@Mock
+	private MachineMasterRepository machineMasterRepository;
 
+	@Before
+	public void initialize() throws Exception {
+		UserContext userContext = Mockito.mock(SessionContext.UserContext.class);
+		PowerMockito.mockStatic(SessionContext.class);
+		PowerMockito.doReturn(userContext).when(SessionContext.class, "userContext");
+		PowerMockito.when(SessionContext.userContext().getUserId()).thenReturn("mosip");
+		PowerMockito.mockStatic(ApplicationContext.class);
+		PowerMockito.when(ApplicationContext.applicationLanguage()).thenReturn("eng");
+
+	}	
 	@Test
 	public void getRegistrationCenterDetailsSuccessTest() {
 
@@ -44,4 +65,31 @@ public class RegistrationCenterDAOTest {
 		assertNotNull(registrationCenterDAOImpl.getRegistrationCenterDetails("mosip","eng"));
 	}
 
+	@Test
+	public void isMachineCenterActiveFalseTest() {
+
+		MachineMaster machineMaster = null;
+		RegistrationCenter registrationCenter = new RegistrationCenter();
+		RegistartionCenterId registartionCenterId = new RegistartionCenterId();
+		registartionCenterId.setId("10011");
+		registrationCenter.setRegistartionCenterId(registartionCenterId);		
+		Optional<RegistrationCenter> registrationCenterList = Optional.of(registrationCenter);
+		Mockito.when(RegistrationSystemPropertiesChecker.getMachineId()).thenReturn("11002");
+		Mockito.when(machineMasterRepository.findByNameIgnoreCase("machineName")).thenReturn(machineMaster);
+		assertTrue(registrationCenterList.isPresent());
+		assertNotNull(registrationCenterDAOImpl.isMachineCenterActive());
+	}
+	
+	@Test
+	public void isMachineCenterActiveTrueTest() {
+		MachineMaster machineMaster = new MachineMaster() ;
+		machineMaster.setName("name");
+		RegistrationCenter registrationCenter = new RegistrationCenter();
+		Optional<RegistrationCenter> registrationCenterList = Optional.of(registrationCenter);
+		Mockito.when(RegistrationSystemPropertiesChecker.getMachineId()).thenReturn("11002");
+		Mockito.when(machineMasterRepository.findByNameIgnoreCase(Mockito.any())).thenReturn(machineMaster);
+		Mockito.when(registrationCenterRepository.findByIsActiveTrueAndRegistartionCenterIdIdAndRegistartionCenterIdLangCode("mosip","eng")).thenReturn(registrationCenterList);
+		assertTrue(registrationCenterList.isPresent());
+		assertNotNull(registrationCenterDAOImpl.isMachineCenterActive());
+	}
 }
