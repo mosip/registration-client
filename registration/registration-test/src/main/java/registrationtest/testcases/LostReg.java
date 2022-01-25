964ed222aaff13db82b65060e32627580e94ff5c
ase-101 <>
2022-01-25 13:13:29 +0530

    security hot fixes

--- registration/registration-test/src/main/java/registrationtest/testcases/LostReg.java
+++ registration/registration-test/src/main/java/registrationtest/testcases/LostReg.java
@@ -212,7 +212,7 @@ public class LostReg {
                 ExtentReportUtil.test1.addScreenCaptureFromPath(WaitsUtil.capture());
             } catch (IOException e1) {
 
-                e1.printStackTrace();
+                logger.error("", e1);
             }
 
         }
