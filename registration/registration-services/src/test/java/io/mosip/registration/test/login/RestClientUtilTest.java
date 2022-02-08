edb7d8d8acc8b4819a2a5003b1b3a5c2294ad754
HimajaDhanyamraju2 <dhanyamraju.himaja2@mindtree.com>
2022-02-08 17:01:30 +0530

    Added test cases

--- registration/registration-services/src/test/java/io/mosip/registration/test/login/RestClientUtilTest.java
+++ registration/registration-services/src/test/java/io/mosip/registration/test/login/RestClientUtilTest.java
@@ -1,16 +1,17 @@
 package io.mosip.registration.test.login;
 
+import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
-import io.mosip.registration.constants.RegistrationConstants;
-import io.mosip.registration.context.ApplicationContext;
-import io.mosip.registration.context.SessionContext;
-import io.mosip.registration.exception.ConnectionException;
-import io.mosip.registration.util.healthcheck.RegistrationSystemPropertiesChecker;
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -22,12 +23,22 @@ import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.client.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RequestCallback;
+import org.springframework.web.client.ResponseExtractor;
+import org.springframework.web.client.RestTemplate;
 
+import io.mosip.registration.constants.RegistrationConstants;
+import io.mosip.registration.context.ApplicationContext;
 import io.mosip.registration.dto.OtpGeneratorRequestDTO;
 import io.mosip.registration.dto.OtpGeneratorResponseDTO;
+import io.mosip.registration.entity.FileSignature;
+import io.mosip.registration.exception.ConnectionException;
 import io.mosip.registration.exception.RegBaseCheckedException;
+import io.mosip.registration.repositories.FileSignatureRepository;
 import io.mosip.registration.util.restclient.RequestHTTPDTO;
 import io.mosip.registration.util.restclient.RestClientUtil;
 
@@ -42,14 +53,30 @@ public class RestClientUtilTest {
 	@InjectMocks
 	RestClientUtil restClientUtil;
 
-	@Mock
-	RequestHTTPDTO requestHTTPDTO;
+	private RequestHTTPDTO requestHTTPDTO;
 
 	@Mock
 	RestTemplate plainRestTemplate;
+	
+	@Mock
+	private FileSignatureRepository fileSignatureRepository;
 
 	@Before
 	public void init() throws Exception {
+		OtpGeneratorResponseDTO generatorResponseDto = new OtpGeneratorResponseDTO();
+		generatorResponseDto.setOtp("099977");
+		OtpGeneratorRequestDTO otpGeneratorRequestDTO = new OtpGeneratorRequestDTO();
+		otpGeneratorRequestDTO.setKey("tutuy");
+		HttpEntity<?> httpEntity = new HttpEntity<OtpGeneratorRequestDTO>(otpGeneratorRequestDTO);
+		URI uri = new URI("https://localhost:8080/otpmanager/otps");
+		requestHTTPDTO = new RequestHTTPDTO();
+		requestHTTPDTO.setClazz(OtpGeneratorResponseDTO.class);
+		requestHTTPDTO.setHttpEntity(httpEntity);
+		requestHTTPDTO.setHttpMethod(HttpMethod.POST);
+		requestHTTPDTO.setUri(uri);
+		requestHTTPDTO.setRequestBody(otpGeneratorRequestDTO);
+		requestHTTPDTO.setHttpHeaders(new HttpHeaders());
+		
 		Map<String,Object> appMap = new HashMap<>();
 		appMap.put(RegistrationConstants.HTTP_API_READ_TIMEOUT, "30");
 		appMap.put(RegistrationConstants.HTTP_API_WRITE_TIMEOUT, "30");
@@ -58,20 +85,43 @@ public class RestClientUtilTest {
 	}
 
 
+	@SuppressWarnings("unchecked")
 	@Test
 	public void invokeHeadersTest() throws RegBaseCheckedException, ConnectionException, URISyntaxException {
-		OtpGeneratorResponseDTO generatorResponseDto = new OtpGeneratorResponseDTO();
-		generatorResponseDto.setOtp("099977");
-		OtpGeneratorRequestDTO otpGeneratorRequestDTO = new OtpGeneratorRequestDTO();
-		otpGeneratorRequestDTO.setKey("tutuy");
-		HttpEntity<?> httpEntity = new HttpEntity<OtpGeneratorRequestDTO>(otpGeneratorRequestDTO);
-		URI uri = new URI("https://localhost:8080/otpmanager/otps");
-		RequestHTTPDTO requestHTTPDTO = new RequestHTTPDTO();
-		requestHTTPDTO.setClazz(OtpGeneratorResponseDTO.class);
-		requestHTTPDTO.setHttpEntity(httpEntity);
-		requestHTTPDTO.setHttpMethod(HttpMethod.POST);
-		requestHTTPDTO.setUri(uri);
-		Assert.assertNull(restClientUtil.invokeURL(requestHTTPDTO));
+		ResponseEntity<?> response = new ResponseEntity<>("Success", HttpStatus.OK);
+		Mockito.when(plainRestTemplate.exchange(Mockito.any(URI.class), Mockito.any(HttpMethod.class), Mockito.any(HttpEntity.class), Mockito.any(Class.class))).thenReturn(response);
+		Assert.assertNotNull(restClientUtil.invokeURL(requestHTTPDTO));
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void invokeForTokenTest() throws URISyntaxException {
+		ResponseEntity<?> response = new ResponseEntity<>("Success", HttpStatus.OK);
+		Mockito.when(plainRestTemplate.exchange(Mockito.any(URI.class), Mockito.any(HttpMethod.class), Mockito.any(HttpEntity.class), Mockito.any(Class.class))).thenReturn(response);
+		Assert.assertNotNull(restClientUtil.invokeForToken(requestHTTPDTO));
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void isConnectedToSyncServerTest() throws MalformedURLException, URISyntaxException {
+		ResponseEntity<?> response = new ResponseEntity<>("Success", HttpStatus.OK);
+		Mockito.when(plainRestTemplate.getForEntity(Mockito.any(URI.class), Mockito.any(Class.class))).thenReturn(response);
+		Assert.assertTrue(restClientUtil.isConnectedToSyncServer("https://localhost:8080/otpmanager/otps"));
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void downloadFileTest() throws Exception {
+		File file = new File(getClass().getClassLoader().getResource("emptyJson.json").getFile());
+		requestHTTPDTO.setFilePath(file.toPath());
+		requestHTTPDTO.setFileEncrypted(true);
+		FileSignature fileSign = new FileSignature();
+		fileSign.setContentLength(10);
+		Optional<FileSignature> fileSignature = Optional.of(fileSign);
+		Mockito.when(fileSignatureRepository.findByFileName(Mockito.anyString())).thenReturn(fileSignature);
+		Mockito.when(fileSignatureRepository.save(Mockito.any(FileSignature.class))).thenReturn(new FileSignature());
+		Mockito.when(plainRestTemplate.execute(Mockito.any(URI.class), Mockito.any(HttpMethod.class), Mockito.any(RequestCallback.class), Mockito.any(ResponseExtractor.class))).thenReturn(file);
+		restClientUtil.downloadFile(requestHTTPDTO);
 	}
 
 }
--- registration/registration-services/src/test/java/io/mosip/registration/test/update/SoftwareUpdateHandlerTest.java
+++ registration/registration-services/src/test/java/io/mosip/registration/test/update/SoftwareUpdateHandlerTest.java
@@ -1,7 +1,12 @@
 package io.mosip.registration.test.update;
 
+import static org.junit.Assert.assertNotNull;
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
 import org.junit.Assert;
@@ -15,19 +20,27 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import com.itextpdf.io.IOException;
+
+import io.mosip.kernel.core.util.FileUtils;
 import io.mosip.registration.constants.RegistrationConstants;
+import io.mosip.registration.context.ApplicationContext;
 import io.mosip.registration.service.config.GlobalParamService;
 import io.mosip.registration.update.SoftwareUpdateHandler;
+import io.mosip.registration.util.restclient.ServiceDelegateUtil;
 
 @RunWith(PowerMockRunner.class)
 @PowerMockIgnore({"com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*", "javax.management.*"})
-@PrepareForTest({Manifest.class})
+@PrepareForTest({Manifest.class, ApplicationContext.class, FileUtils.class})
 public class SoftwareUpdateHandlerTest {
+	
 	@Rule
 	public MockitoRule mockitoRule = MockitoJUnit.rule();
 
@@ -48,40 +61,28 @@ public class SoftwareUpdateHandlerTest {
 	
 	@Mock
 	private Manifest manifest;
+	
+	@Mock
+	private ServiceDelegateUtil serviceDelegateUtil;
 
 	@Before
 	public void initialize() throws Exception {
-		// initMocks(this);
-		//
-		// mockFile = PowerMockito.mock(File.class);
-		//
-		// PowerMockito.whenNew(File.class).withAnyArguments().thenReturn(mockFile);
-		// PowerMockito.whenNew(FileInputStream.class).withAnyArguments().thenReturn(mockFileStream);
-
-		// softwareUpdateHandler = new SoftwareUpdateHandler();
-
+		PowerMockito.mockStatic(ApplicationContext.class, FileUtils.class);
 	}
 
 	@Test
 	public void executeSQLTest() throws Exception {
-
-		// SoftwareUpdateHandler softwareUpdateHandler =new SoftwareUpdateHandler();
-
 		Mockito.doNothing().when(globalParamService).update(Mockito.anyString(), Mockito.anyString());
 
 		Mockito.doNothing().when(jdbcTemplate).execute(Mockito.anyString());
 		Assert.assertSame(RegistrationConstants.SQL_EXECUTION_SUCCESS,
 				softwareUpdateHandler.executeSqlFile("0.11.0", "0.12.5").getSuccessResponseDTO().getMessage());
-
 	}
 
 
 	@Ignore
 	@Test
 	public void executeSQLTestRollBack() throws Exception {
-
-		// SoftwareUpdateHandler softwareUpdateHandler =new SoftwareUpdateHandler();
-
 		Mockito.doNothing().when(globalParamService).update(Mockito.anyString(),
 		Mockito.anyString());
 
@@ -90,7 +91,6 @@ public class SoftwareUpdateHandlerTest {
 
 		SoftwareUpdateHandler softwareUpdateHandle = new SoftwareUpdateHandler();
 		Assert.assertNotNull(softwareUpdateHandle.executeSqlFile("0.11.0", "0.12.5").getErrorResponseDTOs());
-
 	}
 
 	@Test
@@ -103,15 +103,80 @@ public class SoftwareUpdateHandlerTest {
 	public void hasUpdateTest() {
 		Assert.assertFalse(softwareUpdateHandler.hasUpdate());
 	}
-
 	
-/*	@Test
-	public void hasUpdateGetCurrentVersionTest() {
-		
-		Attributes attributes=new Attributes();
-		
+	@Test
+	public void hasUpdateSuccessTest() {
+		ReflectionTestUtils.setField(softwareUpdateHandler, "serverMosipXmlFileUrl", "https://dev.mosip.net/registration-client/maven-metadata.xml");
+		Attributes attributes = new Attributes();
+		attributes.put(Attributes.Name.MANIFEST_VERSION, "1.2.0-rc2-SNAPSHOT");
 		Mockito.when(manifest.getMainAttributes()).thenReturn(attributes);
-		
+		Mockito.when(ApplicationContext.getStringValueFromApplicationMap(RegistrationConstants.MOSIP_UPGRADE_SERVER_URL)).thenReturn("https://dev.mosip.net");
+		Mockito.when(serviceDelegateUtil.prepareURLByHostName(Mockito.anyString())).thenReturn("https://dev.mosip.net/registration-client/maven-metadata.xml");
 		Assert.assertFalse(softwareUpdateHandler.hasUpdate());
-	}*/
+	}
+
+	@Test
+	public void updateDerbyDBTest() {
+		Attributes attributes = new Attributes();
+		attributes.put(Attributes.Name.MANIFEST_VERSION, "1.2.0-rc2-SNAPSHOT");
+		Mockito.when(manifest.getMainAttributes()).thenReturn(attributes);
+		Mockito.when(ApplicationContext.getStringValueFromApplicationMap(Mockito.anyString())).thenReturn("1.2.0-SNAPSHOT");
+		Mockito.doNothing().when(globalParamService).update(Mockito.anyString(), Mockito.anyString());
+		Mockito.doNothing().when(jdbcTemplate).execute(Mockito.anyString());
+		Assert.assertNotNull(softwareUpdateHandler.updateDerbyDB());
+	}
+	
+	@Test
+	public void updateDerbyDBRollBackTest() throws Exception {
+		Attributes attributes = new Attributes();
+		attributes.put(Attributes.Name.MANIFEST_VERSION, "1.2.0-rc2-SNAPSHOT");
+		Mockito.when(manifest.getMainAttributes()).thenReturn(attributes);
+		Mockito.when(ApplicationContext.getStringValueFromApplicationMap(Mockito.anyString())).thenReturn("1.2.0-SNAPSHOT");
+		Mockito.when(ApplicationContext.getStringValueFromApplicationMap(RegistrationConstants.SOFTWARE_BACKUP_FOLDER)).thenReturn("src/test/resources/sql");
+		Mockito.doNothing().when(globalParamService).update(Mockito.anyString(), Mockito.anyString());
+		Mockito.doThrow(IOException.class).when(jdbcTemplate).execute("----- create new tables\n" + 
+				" \n" + 
+				" CREATE TABLE \"REG\".\"LOC_HIERARCHY_LIST\"(\"HIERARCHY_LEVEL\" INTEGER NOT NULL, \"HIERARCHY_LEVEL_NAME\" VARCHAR(36) NOT NULL, \"LANG_CODE\" VARCHAR(3) NOT NULL, \"IS_ACTIVE\" BOOLEAN NOT NULL, \"CR_BY\" VARCHAR(256) NOT NULL, \"CR_DTIMES\" TIMESTAMP NOT NULL, \"UPD_BY\" VARCHAR(256), \"UPD_DTIMES\" TIMESTAMP)");
+		PowerMockito.doNothing().when(FileUtils.class, "copyDirectory", Mockito.any(File.class), Mockito.any(File.class));
+		File backUpFile = new File("src/test/resources/sql");
+		PowerMockito.doNothing().when(FileUtils.class, "copyFile", new File(backUpFile.getAbsolutePath() + "/MANIFEST.MF"), new File("MANIFEST.MF"));
+		Assert.assertNotNull(softwareUpdateHandler.updateDerbyDB());
+	}
+	
+	@Test
+	public void updateDerbyDBNullResponseTest() {
+		Attributes attributes = new Attributes();
+		attributes.put(Attributes.Name.MANIFEST_VERSION, "1.2.0-SNAPSHOT");
+		Mockito.when(manifest.getMainAttributes()).thenReturn(attributes);
+		Mockito.when(ApplicationContext.getStringValueFromApplicationMap(Mockito.anyString())).thenReturn("1.2.0-SNAPSHOT");
+		Mockito.doNothing().when(globalParamService).update(Mockito.anyString(), Mockito.anyString());
+		Mockito.doNothing().when(jdbcTemplate).execute(Mockito.anyString());
+		Assert.assertNull(softwareUpdateHandler.updateDerbyDB());
+	}
+	
+	@Test
+	public void doSoftwareUpgradeTest() throws Exception {
+		Attributes attributes = new Attributes();
+		attributes.put(Attributes.Name.MANIFEST_VERSION, "1.2.0-SNAPSHOT");
+		Mockito.when(manifest.getMainAttributes()).thenReturn(attributes);
+		ReflectionTestUtils.setField(softwareUpdateHandler, "backUpPath", "src/test/resources/sql");
+		ReflectionTestUtils.setField(softwareUpdateHandler, "serverRegClientURL", "https://dev.mosip.net/registration-client/");
+		ReflectionTestUtils.setField(softwareUpdateHandler, "latestVersion", "");
+		PowerMockito.doNothing().when(FileUtils.class, "copyDirectory", Mockito.any(File.class), Mockito.any(File.class));
+		PowerMockito.doNothing().when(FileUtils.class, "deleteDirectory", Mockito.any(File.class));
+		Mockito.doNothing().when(globalParamService).update(Mockito.anyString(), Mockito.anyString());
+		softwareUpdateHandler.doSoftwareUpgrade();
+	}
+	
+	@Test
+	public void getJarChecksumTest() {
+		Attributes attributes = new Attributes();
+		attributes.put(Attributes.Name.CONTENT_TYPE, "test");
+		Map<String, Attributes> entries = new HashMap<>();
+		entries.put("registration-client", attributes);
+		entries.put("registration-services", attributes);
+		Mockito.when(manifest.getEntries()).thenReturn(entries);
+		assertNotNull(softwareUpdateHandler.getJarChecksum());
+	}
+
 }
