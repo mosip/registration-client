adbc9e926db8fa81d39599f66ecb21bf8feca969
M1063027 <M1063027@C1ML45695.mindtree.com>
2022-04-11 18:22:00 +0530

    Modify the code for SONAR fixes

--- registration/registration-services/src/test/java/io/mosip/registration/test/util/mastersync/MapperTest.java
+++ registration/registration-services/src/test/java/io/mosip/registration/test/util/mastersync/MapperTest.java
@@ -3,12 +3,15 @@ package io.mosip.registration.test.util.mastersync;
 import static io.mosip.registration.util.mastersync.MapperUtils.map;
 import static io.mosip.registration.util.mastersync.MetaDataUtils.setCreateMetaData;
 import static io.mosip.registration.util.mastersync.MetaDataUtils.setUpdateMetaData;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
+import org.json.JSONObject;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -17,10 +20,19 @@ import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import io.mosip.kernel.core.authmanager.model.UserRoleDto;
 import io.mosip.registration.context.SessionContext;
 import io.mosip.registration.context.SessionContext.UserContext;
+import io.mosip.registration.dto.mastersync.AppAuthenticationMethodDto;
 import io.mosip.registration.dto.mastersync.LanguageDto;
+import io.mosip.registration.entity.AppAuthenticationMethod;
+import io.mosip.registration.entity.GlobalParam;
 import io.mosip.registration.entity.Language;
+import io.mosip.registration.entity.MachineMaster;
+import io.mosip.registration.entity.RegistrationCommonFields;
+import io.mosip.registration.entity.UserRole;
+import io.mosip.registration.entity.id.AppAuthenticationMethodId;
+import io.mosip.registration.util.mastersync.MapperUtils;
 
 /**
  * 
@@ -47,13 +59,79 @@ public class MapperTest {
 	public void testMapSourceNull() {
 		map(null, new Language());
 	}
-
-
+	
 	@Test(expected = NullPointerException.class)
 	public void testMapMetaDataDestinationNull() {
 		setCreateMetaData(new LanguageDto(), null);
 	}
 
+	@Test
+	public void testMapSource() {
+		LanguageDto dto = getLanguageDto();
+		assertNotNull(map(dto, new Language()));
+
+	}
+
+	@Test
+	public void testMapSourceDTO() {
+		UserRoleDto userRoleDto = new UserRoleDto();
+		userRoleDto.setRole("role");
+		assertNotNull(map(userRoleDto, new UserRole()));
+	}
+
+	@Test
+	public void testMapSourceDTOGenerics() {
+		UserRoleDto userRoleDto = new UserRoleDto();
+		userRoleDto.setRole("role");
+		RegistrationCommonFields userRoleObj = new UserRole();
+		Class<?> userRoleClass = userRoleObj.getClass();
+		assertNotNull(map(userRoleDto, userRoleClass));
+	}
+
+	@Test
+	public void testMapSourceEmbededId() {
+		AppAuthenticationMethod appAuth = new AppAuthenticationMethod();
+		AppAuthenticationMethodId appAuthId = new AppAuthenticationMethodId();
+		appAuthId.setAppId("appId");
+		appAuthId.setAuthMethodCode("authMethodCode");
+		appAuthId.setProcessId("processId");
+		appAuth.setAppAuthenticationMethodId(appAuthId);
+		appAuth.setLangCode("langCode");
+		assertNotNull(map(appAuth, new AppAuthenticationMethodDto()));
+	}
+
+	@Test
+	public void testMapJSONObjectToEntity() throws Exception {
+		JSONObject jsonObject = getJsonObject();
+		AppAuthenticationMethod appAuthenticationMethod = new AppAuthenticationMethod();
+		Class<?> appAuthenticationMethodClass = appAuthenticationMethod.getClass();
+		assertNotNull(MapperUtils.mapJSONObjectToEntity(jsonObject, appAuthenticationMethodClass));
+	}
+
+	@Test
+	public void testMapJSONObjectToEntityFields() throws Exception {
+		JSONObject jsonObject = getJsonObject();
+		AppAuthenticationMethod appAuthenticationMethod = new AppAuthenticationMethod();
+		Class<?> appAuthenticationMethodClass = appAuthenticationMethod.getClass();
+		assertNotNull(MapperUtils.mapJSONObjectToEntity(jsonObject, appAuthenticationMethodClass));
+	}
+
+	@Test
+	public void testMapJSONObjectToEntitySqltimeStamp() throws Exception {
+		JSONObject jsonObject = getSqlTimeJsonObject();
+		GlobalParam globalParam = new GlobalParam();
+		Class<?> globalParamClass = globalParam.getClass();
+		assertNotNull(MapperUtils.mapJSONObjectToEntity(jsonObject, globalParamClass));
+	}
+
+	@Ignore
+	@Test
+	public void testMapJSONObjectToEntityLocaltimeStamp() throws Exception {
+		JSONObject jsonObject = getLocalTimeJsonObject();
+		MachineMaster machineMaster = new MachineMaster();
+		Class<?> machineMasterClass = machineMaster.getClass();
+		assertNotNull(MapperUtils.mapJSONObjectToEntity(jsonObject, machineMasterClass));
+	}
 
 	@Test
 	public void testSetUpdateMetaData() {
@@ -82,4 +160,30 @@ public class MapperTest {
 		assertTrue(entity.getUpdDtimes() != null);
 	}
 
+	private JSONObject getJsonObject() {
+
+		JSONObject jsonObject = new JSONObject(
+				"{'appAuthenticationMethodId':{'appId':'appId','processId':'processId','authMethodCode':'authMethodCode'},'langCode':'langCode'}");
+		return jsonObject;
+	}
+
+	private JSONObject getSqlTimeJsonObject(){
+		JSONObject jsonObject = new JSONObject(
+				"{'globalParamId':{'code':'code','langCode':'langCode'},'isDeleted':true,'delDtimes':'Apr 11, 2022, 3:54:53 PM'}");
+		return jsonObject;
+	}
+	
+	private JSONObject getLocalTimeJsonObject(){
+		JSONObject jsonObject = new JSONObject("{'validityDateTime':{'date':{'year':2022,'month':4,'day':11},'time':{'hour':12,'minute':35,'second':23,'nano':518264300}}}");
+		return jsonObject;
+	}
+	private LanguageDto getLanguageDto() {		
+		LanguageDto dto = new LanguageDto();
+		dto.setCode("ENG");
+		dto.setFamily("English");
+		dto.setIsActive(Boolean.TRUE);
+		dto.setCode("code");
+		return dto;
+	}
+	
 }
