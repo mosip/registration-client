a9388e65bd51c66c9e839e59ad03b6a917d65637
M1044292 <M1044292@C1ML54597.mindtree.com>
2023-03-09 14:30:22 +0530

    Fixed test case

--- registration/registration-services/src/test/java/io/mosip/registration/test/util/RegistrationHealthCheckerTest.java
+++ registration/registration-services/src/test/java/io/mosip/registration/test/util/RegistrationHealthCheckerTest.java
@@ -1,28 +1,17 @@
 package io.mosip.registration.test.util;
 
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 import org.junit.Test;
-import org.mockito.Mock;
+
 import io.mosip.registration.util.healthcheck.RegistrationAppHealthCheckUtil;
-import oshi.software.os.OSFileStore;
 
 public class RegistrationHealthCheckerTest {
 
-@Mock
-private RegistrationAppHealthCheckUtil registrationAppHealthCheckUtil;
-
 	@Test 
-	public void isDiskSpaceAvailableTestTrue() {
-		assertTrue(RegistrationAppHealthCheckUtil.isDiskSpaceAvailable());
-			
-	}
-	
-	@Test
-	public void isDiskSpaceAvailableTestFalse() {
-		OSFileStore fs = new OSFileStore();
-		fs.setUsableSpace(0);
-		
+	public void isDiskSpaceAvailableTest() {
+		assertFalse(RegistrationAppHealthCheckUtil.isDiskSpaceAvailable());		
 	}
 	
 }
