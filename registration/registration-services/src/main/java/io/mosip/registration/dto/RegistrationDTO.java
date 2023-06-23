b38fbe226ebc201c86e20171b0b5c1771e8ad9ec
Himaja Dhanyamraju <43470317+HimajaDhanyamraju2@users.noreply.github.com>
2023-06-23 22:08:45 +0530

    Biometrics Changes (#567)
    * MOSIP-27529: MOSIP-27809, MOSIP-27684 (#562)
    
    * MOSIP-27809: Upgrade changes
    
    * Fixed MOSIP-27684
    
    * MOSIP-27809: Handling inputstream
    
    * Modified logging
    
    * Added error log in catch block
    
    ---------
    
    Co-authored-by: M1044292 <M1044292@C1ML54597.mindtree.com>
    # Conflicts:
    #	registration/registration-services/src/test/java/io/mosip/registration/test/update/SoftwareUpdateHandlerTest.java
    
    * Added default-version-mappings
    
    * Fixed test case
    
    * Display MDS and SDK quality when sdk quality check is enabled
    
    # Conflicts:
    #	registration/registration-client/src/main/java/io/mosip/registration/controller/GenericController.java
    #	registration/registration-client/src/main/java/io/mosip/registration/controller/device/GenericBiometricsController.java
    #	registration/registration-client/src/main/resources/application.css
    #	registration/registration-client/src/main/resources/fxml/BiometricsCapture.fxml
    #	registration/registration-services/src/main/java/io/mosip/registration/dto/RegistrationDTO.java
    
    * Handling exception photo scan with new MDS
    
    * Biometrics screen alignment modifications
    
    * Rollback BiometricsCapture.fxml
    
    * Modified biometrics screen alignments
    
    * Rolled back RegistrationDTO.java
    
    * Changes to display MDS and SDK score
    
    * Changes to display MDS and SDK score
    
    * Rolledback GenericBiometricsController.java
    
    * Changes to display MDS and SDK scores
    
    ---------
    
    Co-authored-by: M1044292 <M1044292@C1ML54597.mindtree.com>
    Co-authored-by: Rediet <rediet@mosip.io>

--- registration/registration-services/src/main/java/io/mosip/registration/dto/RegistrationDTO.java
+++ registration/registration-services/src/main/java/io/mosip/registration/dto/RegistrationDTO.java
@@ -76,6 +76,7 @@ public class RegistrationDTO {
 	// Caches
 	public Map<String, byte[]> BIO_CAPTURES = new HashMap<>();
 	public Map<String, Double> BIO_SCORES = new HashMap<>();
+	public Map<String, Double> SDK_SCORES = new HashMap<>();
 	public Map<String, Object> AGE_GROUPS = new HashMap<>();
 	public Map<String, Integer> ATTEMPTS = new HashMap<>();
 	public Map<String, List<String>> CONFIGURED_BIOATTRIBUTES = new HashMap<>();
@@ -90,6 +91,7 @@ public class RegistrationDTO {
 		this.biometricExceptions.clear();
 		this.BIO_CAPTURES.clear();
 		this.BIO_SCORES.clear();
+		this.SDK_SCORES.clear();
 		this.ATTEMPTS.clear();
 		this.SELECTED_CODES.clear();
 
@@ -230,20 +232,28 @@ public class RegistrationDTO {
 		this.biometrics.remove(key);
 		key = String.format("%s_%s", fieldId, Modality.EXCEPTION_PHOTO.name());
 		this.ATTEMPTS.remove(key);
-		Iterator<Entry<String, byte[]>> iterator = this.BIO_CAPTURES.entrySet().iterator();
-		while (iterator.hasNext()) {
-		    Entry<String, byte[]> item = iterator.next();
+		Iterator<Entry<String, byte[]>> bioCapturesIterator = this.BIO_CAPTURES.entrySet().iterator();
+		while (bioCapturesIterator.hasNext()) {
+		    Entry<String, byte[]> item = bioCapturesIterator.next();
 		    if (item.getKey().startsWith(String.format("%s_%s_", fieldId, RegistrationConstants.notAvailableAttribute))) {
-		    	iterator.remove();
+		    	bioCapturesIterator.remove();
 		    }
 		}
 
-		Iterator<Entry<String, Double>> iterator2 = this.BIO_SCORES.entrySet().iterator();
-		while (iterator2.hasNext()) {
-		    Entry<String, Double> item = iterator2.next();
+		Iterator<Entry<String, Double>> bioScoresIterator = this.BIO_SCORES.entrySet().iterator();
+		while (bioScoresIterator.hasNext()) {
+		    Entry<String, Double> item = bioScoresIterator.next();
 		    if (item.getKey().startsWith(String.format("%s_%s_", fieldId, Modality.EXCEPTION_PHOTO.name()))) {
-		    	iterator2.remove();
+		    	bioScoresIterator.remove();
 		    }
+		}
+
+		Iterator<Entry<String, Double>> sdkScoresIterator = this.SDK_SCORES.entrySet().iterator();
+		while (sdkScoresIterator.hasNext()) {
+			Entry<String, Double> item = sdkScoresIterator.next();
+			if (item.getKey().startsWith(String.format("%s_%s_", fieldId, Modality.EXCEPTION_PHOTO.name()))) {
+				sdkScoresIterator.remove();
+			}
 		}
 	}
 
@@ -259,6 +269,7 @@ public class RegistrationDTO {
 					.filter( k -> k.startsWith(String.format("%s_%s", fieldId, attr)))
 					.forEach( k -> {
 						this.BIO_SCORES.remove(k);
+						this.SDK_SCORES.remove(k);
 						this.BIO_CAPTURES.remove(k);
 						this.biometrics.remove(k);
 						this.biometricExceptions.remove(k);
@@ -270,10 +281,12 @@ public class RegistrationDTO {
 
 		keys.clear();
 		keys.addAll(this.BIO_SCORES.keySet());
+		keys.addAll(this.SDK_SCORES.keySet());
 		keys.stream()
 				.filter( k -> k.startsWith(key))
 				.forEach( k -> {
 					this.BIO_SCORES.remove(k);
+					this.SDK_SCORES.remove(k);
 				});
 	}
 
