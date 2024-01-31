25221740e316b7ed32c65bf119e37d14d394ed2b
HimajaDhanyamraju2 <dhanyamraju.himaja2@mindtree.com>
2024-01-31 11:37:56 +0530

    MOSIP-31328: Rolled back sonar fix
    Signed-off-by: HimajaDhanyamraju2 <dhanyamraju.himaja2@mindtree.com>

--- registration/registration-services/src/main/java/io/mosip/registration/mdm/service/impl/MosipDeviceSpecificationHelper.java
+++ registration/registration-services/src/main/java/io/mosip/registration/mdm/service/impl/MosipDeviceSpecificationHelper.java
@@ -278,16 +278,18 @@ public class MosipDeviceSpecificationHelper {
 	public CloseableHttpResponse getHttpClientResponse(String url, String method, String body) throws IOException {
 		int timeout = getMDMConnectionTimeout(method);
 		LOGGER.debug("MDM HTTP CALL method : {}  with timeout {}", method, timeout);
-		try (CloseableHttpClient client = HttpClients.createDefault()) {
-			RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(timeout).setSocketTimeout(timeout)
-					.setConnectionRequestTimeout(timeout).build();
-			StringEntity requestEntity = new StringEntity(body, ContentType.create("Content-Type", Consts.UTF_8));
-			HttpUriRequest httpUriRequest = RequestBuilder.create(method).setConfig(requestConfig).setUri(url)
-					.setEntity(requestEntity).build();
-			return client.execute(httpUriRequest);
-		} catch (Exception exception) {
-			LOGGER.error("MDM HTTP call failed", exception);
-			return null;
-		}
+		RequestConfig requestConfig = RequestConfig.custom()
+				.setConnectTimeout(timeout)
+				.setSocketTimeout(timeout)
+				.setConnectionRequestTimeout(timeout)
+				.build();
+		CloseableHttpClient client = HttpClients.createDefault();
+		StringEntity requestEntity = new StringEntity(body, ContentType.create("Content-Type", Consts.UTF_8));
+		HttpUriRequest httpUriRequest = RequestBuilder.create(method)
+				.setConfig(requestConfig)
+				.setUri(url)
+				.setEntity(requestEntity)
+				.build();
+		return client.execute(httpUriRequest);
 	}
 }
