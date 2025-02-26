186355af97562eb1ea063cde1efb8f3125b6c226
GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
2025-02-26 13:51:18 +0530

    Fixed Ignored test cases
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- registration/registration-services/src/test/java/io/mosip/registration/test/update/ClientIntegrityValidatorTest.java
+++ registration/registration-services/src/test/java/io/mosip/registration/test/update/ClientIntegrityValidatorTest.java
@@ -89,10 +89,9 @@ public class ClientIntegrityValidatorTest {
 	
 	}
 
-	@Ignore //Skipped due to trusted certificate is expired
 	@Test
 	public void integrityCheckTest() throws IOException {
-		URL url = ManifestCreatorTest.class.getResource("/setup/registration-api-1.2.0-SNAPSHOT.jar");
+		URL url = ManifestCreatorTest.class.getResource("/setup/registration-api-1.3.0-SNAPSHOT.jar");
 		X509Certificate certificate = ClientIntegrityValidator.getCertificate();
 		JarFile jarFile = new JarFile(url.getFile());
 		ClientIntegrityValidator.verifyIntegrity(certificate, jarFile);
