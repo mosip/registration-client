203aebf018bb020fbd8d4b824ca3d059b956acf4
nagendra0721 <nagendra0718@gmail.com>
2025-03-19 11:25:04 +0530

    MOSIP-39775: db script - reg-client-develop (#706)
    * MOSIP-39775: db script - reg-client-develop
    
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>
    
    * MOSIP-39775: db script - reg-client-develop
    
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>
    
    ---------
    
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>

--- registration/registration-services/src/main/resources/sql/1.2.1/rollback_scripts.sql
+++ registration/registration-services/src/main/resources/sql/1.2.1/rollback_scripts.sql
@@ -1,3 +1,5 @@
 ----- 
 
 UPDATE "REG"."GLOBAL_PARAM" set VAL='AES/GCM/PKCS5Padding' WHERE CODE='mosip.kernel.crypto.symmetric-algorithm-name';
+
+ALTER TABLE IF EXISTS "REG"."CA_CERT_STORE" DROP COLUMN "CA_CERT_TYPE";
