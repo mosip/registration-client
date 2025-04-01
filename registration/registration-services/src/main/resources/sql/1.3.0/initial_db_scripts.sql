70fe7b2f2c3b236b7e8e2fc116752f7b546ebf6c
Gokulraj C <110164849+GOKULRAJ136@users.noreply.github.com>
2025-04-01 11:00:09 +0530

    MOSIP-39775: added CA_CERT_TYPE in initial.sql (#708) (#710)
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- registration/registration-services/src/main/resources/sql/1.3.0/initial_db_scripts.sql
+++ registration/registration-services/src/main/resources/sql/1.3.0/initial_db_scripts.sql
@@ -1,3 +1,5 @@
 ----- insert / update statements
 
 UPDATE "REG"."GLOBAL_PARAM" set VAL='AES/GCM/NoPadding' WHERE CODE='mosip.kernel.crypto.symmetric-algorithm-name';
+
+ALTER TABLE "REG"."CA_CERT_STORE" ADD COLUMN "CA_CERT_TYPE" VARCHAR(25);
