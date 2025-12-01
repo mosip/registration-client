fe202e708ef4b09cd232c9abc9a7f0eca966358b
ase-101 <sunkadaeanusha@gmail.com>
2025-12-01 18:17:35 +0530

    Corrected the DB upgrade scripts
    Signed-off-by: ase-101 <sunkadaeanusha@gmail.com>

--- registration/registration-services/src/main/resources/sql/1.3.0-B2/rollback_scripts.sql
+++
@@ -1,3 +0,0 @@
--- Rollback script for version 1.3.0-B2
-
-ALTER TABLE "REG"."CA_CERT_STORE" DROP COLUMN "CA_CERT_TYPE";
--- registration/registration-services/src/main/resources/sql/1.3.0/rollback_scripts.sql
+++ registration/registration-services/src/main/resources/sql/1.3.0/rollback_scripts.sql
@@ -1,3 +1,3 @@
------ 
-
+-- Rollback script for version 1.3.0
+ALTER TABLE "REG"."CA_CERT_STORE" DROP COLUMN "CA_CERT_TYPE";
 UPDATE "REG"."GLOBAL_PARAM" set VAL='AES/GCM/PKCS5Padding' WHERE CODE='mosip.kernel.crypto.symmetric-algorithm-name';
