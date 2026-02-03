75db17efec52d3d94719ef12aac49e81f7313aa2
Gokulraj C <110164849+GOKULRAJ136@users.noreply.github.com>
2026-02-03 18:53:26 +0530

    Added CA Certificate validity [MOSIP-43922] (#759)
    * Added CA Certificate validity [MOSIP-43922]
    
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
    
    * DB version changes
    
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- registration/registration-services/src/main/resources/sql/1.2.1/initial_db_scripts.sql
+++
@@ -1,5 +0,0 @@
------ insert / update statements
-
-UPDATE "REG"."GLOBAL_PARAM" set VAL='AES/GCM/NoPadding' WHERE CODE='mosip.kernel.crypto.symmetric-algorithm-name';
-
-ALTER TABLE "REG"."CA_CERT_STORE" ADD COLUMN "CA_CERT_TYPE" VARCHAR(25);
