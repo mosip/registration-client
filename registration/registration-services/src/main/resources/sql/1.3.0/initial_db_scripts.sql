46ca45311f2ffdb03eb01ea6d5f268ff4e798d3c
Gokulraj C <110164849+GOKULRAJ136@users.noreply.github.com>
2026-07-03 12:23:04 +0530

    Corrected the DB upgrade scripts #807 (#823)
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- registration/registration-services/src/main/resources/sql/1.3.0/initial_db_scripts.sql
+++ registration/registration-services/src/main/resources/sql/1.3.0/initial_db_scripts.sql
@@ -1,5 +1,5 @@
------ insert / update statements
-
+-- Upgrade script for version 1.3.0
+ALTER TABLE "REG"."CA_CERT_STORE" ADD COLUMN "CA_CERT_TYPE" VARCHAR(25);
 UPDATE "REG"."GLOBAL_PARAM" set VAL='AES/GCM/NoPadding' WHERE CODE='mosip.kernel.crypto.symmetric-algorithm-name';
 
 MERGE INTO "REG"."GLOBAL_PARAM" gp1 USING "SYSIBM"."SYSDUMMY1" ON gp1.code = 'mosip.kernel.partner.cacertificate.upload.minimumvalidity.month' WHEN NOT MATCHED THEN INSERT VALUES ('mosip.kernel.partner.cacertificate.upload.minimumvalidity.month', 'mosip.kernel.partner.cacertificate.upload.minimumvalidity.month', '1', 'CONFIGURATION', 'eng', true, 'SYSTEM', current timestamp, 'SYSTEM', current timestamp, false, current timestamp);
