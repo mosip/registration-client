1acb03599d1e8c9c793402c34c65c23ef8863706
Yakshith K D <yakshithkd97@gmail.com>
2026-07-06 17:00:37 +0530

    feat(telemetry): wire desktop Registration Client metrics to TUSD ingestion backend
    Signed-off-by: Yakshith K D <yakshithkd97@gmail.com>

--- registration/registration-services/src/main/java/io/mosip/registration/config/AppConfig.java
+++ registration/registration-services/src/main/java/io/mosip/registration/config/AppConfig.java
@@ -2,37 +2,31 @@ package io.mosip.registration.config;
 
 import javax.sql.DataSource;
 
-import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+import io.mosip.commons.packet.facade.PacketWriter;
 import io.mosip.kernel.auditmanager.config.AuditConfig;
 import io.mosip.kernel.core.logger.spi.Logger;
+
+import io.mosip.kernel.core.templatemanager.spi.TemplateManagerBuilder;
 import io.mosip.kernel.dataaccess.hibernate.repository.impl.HibernateRepositoryImpl;
+import io.mosip.kernel.logger.logback.appender.RollingFileAppender;
 import io.mosip.kernel.logger.logback.factory.Logfactory;
 import io.mosip.kernel.templatemanager.velocity.builder.TemplateManagerBuilderImpl;
 
@@ -47,6 +41,7 @@ import io.mosip.kernel.templatemanager.velocity.builder.TemplateManagerBuilderIm
 @EnableAspectJAutoProxy
 @Import({ DaoConfig.class, AuditConfig.class, TemplateManagerBuilderImpl.class })
 @EnableJpaRepositories(basePackages = "io.mosip.registration", repositoryBaseClass = HibernateRepositoryImpl.class)
+@EnableScheduling   // these one added 
 @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = {
 		".*IdObjectCompositeValidator",
 		".*IdObjectMasterDataValidator",
@@ -57,7 +52,7 @@ import io.mosip.kernel.templatemanager.velocity.builder.TemplateManagerBuilderIm
 		"io.mosip.kernel.idvalidator", "io.mosip.kernel.ridgenerator", "io.mosip.kernel.qrcode",
 		"io.mosip.kernel.crypto", "io.mosip.kernel.jsonvalidator", "io.mosip.kernel.idgenerator",
 		"io.mosip.kernel.virusscanner", "io.mosip.kernel.transliteration", "io.mosip.kernel.applicanttype",
-		"io.mosip.kernel.core.pdfgenerator.spi", "io.mosip.kernel.pdfgenerator.impl",
+		"io.mosip.kernel.core.pdfgenerator.spi", "io.mosip.kernel.pdfgenerator.itext.impl",
 		"io.mosip.kernel.idobjectvalidator.impl", "io.mosip.kernel.biosdk.provider.impl",
 		"io.mosip.kernel.biosdk.provider.factory", "io.mosip.commons.packet",
 		"io.mosip.registration.api.config" })
@@ -66,18 +61,6 @@ import io.mosip.kernel.templatemanager.velocity.builder.TemplateManagerBuilderIm
 @EnableConfigurationProperties
 @EnableRetry
 public class AppConfig {
-	
-	@Value("${regclient.default.httpclient.connections.max.per.host:20}")
-	private int defaultMaxConnectionPerRoute;
-
-	@Value("${regclient.default.httpclient.connections.max:100}")
-	private int defaultTotalMaxConnection;
-	
-	@Value("${regclient.selfToken.httpclient.connections.max.per.host:20}")
-	private int selfTokenMaxConnectionPerRoute;
-
-	@Value("${regclient.selfToken.httpclient.connections.max:100}")
-	private int selfTokenTotalMaxConnection;
 
 	@Autowired
 	@Qualifier("dataSource")
@@ -90,22 +73,12 @@ public class AppConfig {
 	@Bean
 	@Primary
 	public RestTemplate restTemplate() {
-		HttpClientBuilder httpClientBuilder = HttpClients.custom().setConnectionManager(PoolingHttpClientConnectionManagerBuilder.create().setMaxConnPerRoute(defaultMaxConnectionPerRoute)
-				.setMaxConnTotal(defaultTotalMaxConnection).build()).disableCookieManagement();
-		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-		requestFactory.setHttpClient(httpClientBuilder.build());
-
-		return new RestTemplate(requestFactory);
+		return new RestTemplate();
 	}
 
 	@Bean
 	public RestTemplate selfTokenRestTemplate() {
-		HttpClientBuilder httpClientBuilder = HttpClients.custom().setConnectionManager(PoolingHttpClientConnectionManagerBuilder.create().setMaxConnPerRoute(selfTokenMaxConnectionPerRoute)
-				.setMaxConnTotal(selfTokenTotalMaxConnection).build()).disableCookieManagement();
-		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-		requestFactory.setHttpClient(httpClientBuilder.build());
-
-		return new RestTemplate(requestFactory);
+		return new RestTemplate();
 	}
 
 	@Bean
